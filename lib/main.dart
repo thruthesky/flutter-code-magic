4be195da0fea1838d8044f69ffc01104bbcfd766
JaeHo Song <thruthesky@gmail.com>
2020-01-21 15:47:12 +0800

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Code Magic Test 5'),
+      home: MyHomePage(title: 'Flutter Code Magic Test 6'),
     );
   }
 }
